6a05e93d8226af64abb679cc6ad47277f5a6879c
IvoryMay <theintsandikyaw1@gmail.com>
2025-12-16 22:48:47 +0630

    no image option added

--- src/components/MovieDetailSection.jsx
+++ src/components/MovieDetailSection.jsx
@@ -11,7 +11,7 @@ const MovieDetailSection = ({data}) => {
     <section className='mt-10'>
       <div className=' sm:p-4 rounded-lg flex flex-col md:flex-row gap-5 md:justify-center md:items-center md:gap-10'>
       
-      <Image src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} alt={data.title} width={400} height={150} className='rounded-lg  mb-2'/>
+      {data.poster_path?<Image src={`https://image.tmdb.org/t/p/original/${data.poster_path}`} alt={data.title} width={400} height={150} className='rounded-lg  mb-2'/>:<div className="w-[400px] h-[150px] rounded-lg group-hover:opacity-75 transition-opacity duration-300 mb-2 dark:bg-gray-500  bg-gray-300 flex items-center justify-center">No Image</div>}
       <div>
         <h1 className=' font-bold mb-2'>{data.title}</h1>
         <p className='mb-2 font-semibold'>Genre : {data.genres && data.genres.map((genre) => genre.name).join(', ')}</p>
